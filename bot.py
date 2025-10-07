edf13d618b5de1bc0cdffaddc63a102efd34572b
Botan0508 <55D@ukr.net>
2025-10-08 01:33:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
 # Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой виртуальный менеджер по недвижимости!")
 
 def main():
-    # ВСТАВЬ СВОЙ ТОКЕН
-    import os
     app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
-
     app.add_handler(CommandHandler("start", start))
 
     print("✅ Бот запущен и ожидает команды...")
@@ -17,5 +15,3 @@ def main():
 
 if name == "main":
     main()
-
-

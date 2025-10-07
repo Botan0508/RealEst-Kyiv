a6f5f760e1d92c60239ba2eb8cff96343b754264
Botan0508 <55D@ukr.net>
2025-10-08 02:49:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,18 +7,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой виртуальный менеджер по недвижимости!")
 
 def main():
-    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
-    app.add_handler(CommandHandler("start", start))
+    token = os.getenv("BOT_TOKEN")
+    if not token:
+        
+        return
 
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
     print("✅ Бот запущен и ожидает команды...")
     app.run_polling()
 
-if __name__ == "__main__":
+if name == "main":
     main()
-    
-
-
-
 
 
 
